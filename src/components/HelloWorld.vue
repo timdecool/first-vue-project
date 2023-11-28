28c0ad153d08f2810d5cb2762c0f1a89300d7c7d
Tim Decool <tim.decool@gmail.com>
2023-11-28 13:50:07 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: [String,Number],
+      required: true,
+      default: function() {
+        return "Bonjour !"
+      }
+    }
+  },
+  data() {
+    return {count: 0}
+  }
+}
+</script>
+
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
